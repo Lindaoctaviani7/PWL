757de8cf58c1381fdc77c60af5e5ff348813d6bc
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2026-02-23 15:44:42 +0700

    prak 1 dan 2(1-5)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/world', function () {
+    return 'World';
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/about', function () {
+    return 'NIM: 244107020002 - Nama: Linda Octaviani';
+});
+
+Route::get('/user/{nama}', function ($nama) {
+    return 'Nama saya ' . $nama;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+
